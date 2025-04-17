5265534d76e61d5cee9cd7819e8a2e203fb7e1b4
shan09git <shanjob09@gmail.com>
2025-04-17 01:53:14 -0400

    commit init

---
+++ my-app-users/src/main/java/app/MyAppUsersApplication.java
@@ -0,0 +1,13 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAppUsersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyAppUsersApplication.class, args);
+    }
+
+}
