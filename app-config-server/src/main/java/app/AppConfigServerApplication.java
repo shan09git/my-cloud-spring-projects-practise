9605cd89c885db41d93d3fdfa543827452ba8b83
shan09git <shanjob09@gmail.com>
2025-04-20 20:16:58 -0400

    first commit.

---
+++ app-config-server/src/main/java/app/AppConfigServerApplication.java
@@ -0,0 +1,13 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppConfigServerApplication.class, args);
+    }
+
+}
