20cfb0042cc2157ab078271a57f6efc638feaef6
shan09git <shanjob09@gmail.com>
2025-04-20 20:19:31 -0400

    added config and properties.

--- app-config-server/src/main/java/app/AppConfigServerApplication.java
+++ app-config-server/src/main/java/app/AppConfigServerApplication.java
@@ -2,8 +2,10 @@ package app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class AppConfigServerApplication {
 
     public static void main(String[] args) {
